da0a7cb520373567f88165441c8c2f0553a0deff
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-08-26 01:18:26 -0400

    dont console log get

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ var friendsData = require("../data/friends");
 module.exports = function(app) {
   
   app.get("/api/friends", function(req, res) {
-    console.log(res.json(friendsData));
+    res.json(friendsData);
   });
 
 
