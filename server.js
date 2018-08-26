326e8fa3afcd3f266baa38931879d89ad6722e22
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-08-26 02:07:19 -0400

    added stylesheet and path npm

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-
+var path = require("path");
 var PORT = process.env.PORT || 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./app/public")));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
