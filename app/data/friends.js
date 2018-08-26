811d659c2f7747038e682bdb609120e3a9a40f13
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-08-26 02:48:43 -0400

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,6 +17,57 @@ var friendsArray = [
       
     },
     {
+        "name":"Checon",
+        "photo":"http://portalradar.com.br/portal-radar/wp-content/uploads/2014/06/Marcelo-Checon-560x314.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+      
+    },
+    {
+        "name":"Dree",
+        "photo":"http://smashinginterviews.com/wp-content/uploads/2018/04/DreeHemingwayFeatured.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      
+    },
+    {
+        "name":"Kour",
+        "photo":"https://static.wixstatic.com/media/58af84_6e5ec8a729f442208db2498423282a71~mv2_d_3840_5760_s_4_2.jpg/v1/crop/x_0,y_323,w_3840,h_3840/fill/w_380,h_380,al_c,q_80,usm_0.66_1.00_0.01/58af84_6e5ec8a729f442208db2498423282a71~mv2_d_3840_5760_s_4_2.webp",
+        "scores":[
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+          ]
+      
+    },
+    {
         "name":"Min",
         "photo":"http://i.eyeanet.com/wp-content/uploads/2018/01/thong-tin-ca-si-min.jpg",
         "scores":[
